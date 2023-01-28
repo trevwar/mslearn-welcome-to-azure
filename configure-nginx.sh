cb63e46e57d6df794b800ac468e3bff9118b1c78
trevwar <trevwar@users.noreply.github.com>
2023-01-28 10:06:15 +0000

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Welcome to Azure Karen! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
